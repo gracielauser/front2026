0ea35e57c19c9b5268fc92c7e500c1419fcea2cf
gracielauser <gracielapinedo0000@gmail.com>
2026-03-08 17:42:07 -0400

    ngrok server local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://unimmersed-gonidial-bell.ngrok-free.dev'  // tu backend real
+  // apiUrl: 'https://backend2026-jqwt.onrender.com'  // tu backend real
+  apiUrl: 'https://unimmersed-gonidial-bell.ngrok-free.dev'
 };
