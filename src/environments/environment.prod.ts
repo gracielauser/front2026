63505d2d325fb0fefab518aba4c261f5c8b9ac62
gracielauser <gracielapinedo0000@gmail.com>
2026-03-08 17:58:56 -0400

    tunnel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://backend2026-jqwt.onrender.com'  // tu backend real
-  apiUrl: 'https://unimmersed-gonidial-bell.ngrok-free.dev'
+  apiUrl: 'https://b47blm-ip-181-115-161-172.tunnelmole.net'
 };
