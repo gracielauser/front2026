ffba3d1779f729d169952d452470c9117a2a5a40
gracielauser <gracielapinedo0000@gmail.com>
2026-03-09 13:05:18 -0400

    url de render actualizada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  // apiUrl: 'https://backend2026-jqwt.onrender.com'  // tu backend real
-  apiUrl: 'https://dkzovx-ip-190-129-17-28.tunnelmole.net'
+  apiUrl: 'https://backend2026-jqwt.onrender.com'  // tu backend real
+  // apiUrl: 'https://dkzovx-ip-190-129-17-28.tunnelmole.net'
 };
