6b610ed2c1f8b6148bf166bb40e606bb9d4a6522
gracielauser <gracielapinedo0000@gmail.com>
2026-03-08 20:40:05 -0400

    ruta de tunnel mole

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://backend2026-jqwt.onrender.com'  // tu backend real
-  apiUrl: process.env['apiURL'] || 'http://localhost:8080' // Usa la variable de entorno o localhost como fallback
+  apiUrl: 'https://qhapcc-ip-190-129-17-26.tunnelmole.net'
 };
